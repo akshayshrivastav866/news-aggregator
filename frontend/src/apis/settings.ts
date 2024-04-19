43c65df5c2042fc53a600259153c6b55db2773ca
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-04-19 17:49:29 +0530

    FE Endpoint setup

--- frontend/src/apis/settings.ts
+++ frontend/src/apis/settings.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:800/api';
+axios.defaults.baseURL = 'http://127.0.0.1:8001/api';
 
 axios.defaults.headers.common['Authorization'] = 'token';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
