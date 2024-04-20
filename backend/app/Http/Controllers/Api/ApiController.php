b0487aef66e67c859a87f438add4944605b05e48
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-04-20 20:56:30 +0530

    Updated structure and setup base API controller

---
+++ backend/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+    {
+        public function register( Request $request ) {
+
+        }
+
+        public function login( Request $request ) {
+
+        }
+
+        public function logout() {
+
+        }
+    }
