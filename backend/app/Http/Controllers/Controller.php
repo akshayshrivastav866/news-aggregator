91fcd7e4b88f3ff3ee0f7b5274ef86a2fa3d2978
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-04-24 14:50:13 +0530

    BE - Updated main controller

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -3,12 +3,6 @@
 namespace App\Http\Controllers;
 
 abstract class Controller {
-    protected $defined_preferences = [
-        'categories' => [ 'business', 'health', 'general', 'science', 'sports' ],
-        'authors' => [ 'Javier Lacort', 'Uriel Bederman', 'Simon Cohen' ],
-        'sources' => [ 'CNET', 'MacRumors', 'Hipertextual', 'Digital Trends', 'Yahoo Entertainment' ],
-    ];
-
     protected $api_keys = [
         'newsapi' => '',
         'guardian' => '',
