7fed6fb276892d0dd947c44f1de6747b264f49cd
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-04-24 18:53:46 +0530

    BE - Updated feedsController

--- backend/app/Http/Controllers/Api/FeedsController.php
+++ backend/app/Http/Controllers/Api/FeedsController.php
@@ -5,35 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Feeds;
+use App\Models\UserPreferences;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\AUth;
 
 class FeedsController extends Controller {
     public function filters() {
         return response()->json([
             'status' => 200,
             'message' => '',
-            'data' => $this->defined_preferences
+            'data' => $this->defined_preferences()
         ]);
     }
 
-    // Return results based on search keywords
-    public function getSearchedResults( Request $request ) {
-        $searchQuery = $request->query('query');
-
-        if ( $searchQuery === null ) {
-            return response()->json([
-                'status' => 200,
-                'message' => 'Please search something to get news!',
-                'data' => [],
-            ]);
+    // We want to send a structured data to FE so that it can easily parse without any additional logic and display data.
+    private function getParsedResponse( $responses ) {
+        if ( empty( $responses ) ) {
+            return [];
         }
 
-        $responses = Http::pool(function ($pool) use ($searchQuery) {
-            $pool->get( 'https://newsapi.org/v2/everything?q=' . $searchQuery . '&pageSize=10&apiKey=' . $this->api_keys['newsapi'] );
-            $pool->get( 'https://content.guardianapis.com/search?q=' . $searchQuery . '&show-elements=image&page-size=10&api-key=' . $this->api_keys['guardian'] );
-        });
-
-        // We want to send a structured data to FE so that it can easily parse without any additional logic and display data.
         $finalArray = [];
 
         foreach ($responses as $response) {
@@ -49,7 +39,7 @@ class FeedsController extends Controller {
                             'url' => $article['url'] ?? null,
                             'thumbnail' => $article['urlToImage'] ?? null,
                             'source' => $article['source']['name'] ?? null,
-                            'apiSource' => 'newsAPI'
+                            'apiSource' => 'News API'
                         ];
                     }
                 }
@@ -64,24 +54,87 @@ class FeedsController extends Controller {
                         'url' => $result['webUrl'] ?? null,
                         'thumbnail' => $result['elements'][1]['assets'][0]['file'] ?? $result['elements'][0]['assets'][0]['file'] ?? null,
                         'source' => null,
-                        'apiSource' => 'guardian'
+                        'apiSource' => 'Guardian API'
                     ];
                 }
             }
         }
 
+        return $finalArray;
+    }
+
+    // Return results based on search keywords
+    public function getSearchedResults( Request $request ) {
+        $searchQuery = $request->query('query');
+
+        if ( $searchQuery === null ) {
+            return response()->json([
+                'status' => 200,
+                'message' => 'Please search something to get news!',
+                'data' => [],
+            ]);
+        }
+
+        $responses = Http::pool(function ($pool) use ($searchQuery) {
+            $pool->get('https://newsapi.org/v2/everything', [
+                'q' => $searchQuery,
+                'pageSize' => 10,
+                'apiKey' => $this->api_keys['newsapi']
+            ]);
+            $pool->get('https://content.guardianapis.com/search', [
+                'q' => $searchQuery,
+                'show-elements' => 'image',
+                'page-size' => 10,
+                'api-key' => $this->api_keys['guardian']
+            ]);
+        });
+
         return response()->json([
             'status' => 200,
-            'message' => 'Search Results',
-            'data' => $finalArray
+            'message' => 'Your hot Search Results!!',
+            'data' => $this->getParsedResponse( $responses )
         ]);
     }
 
     public function getDefaultFeeds() {
+        $userId = Auth::id();
+        $preferences = '';
+
+        $userPreferences = UserPreferences::where('user_id', $userId)->first();
+
+        if ( $userPreferences !== null ) {
+            $categories = $userPreferences->preferences['categories'];
+
+            foreach ( $categories as $index => $category ) {
+                $preferences .= $category;
+
+                if ($index < count($categories) - 1) {
+                    $preferences .= ',';
+                }
+            }
+        } else {
+            $preferences = 'business,technology,sports,science,health,general,sports';
+        }
+
+        $responses = Http::pool(function ($pool) use ($preferences) {
+            $pool->get('https://newsapi.org/v2/everything', [
+                'q' => explode( ',', $preferences )[0],
+                'pageSize' => 15,
+                'apiKey' => $this->api_keys['newsapi']
+            ]);
+            $pool->get('https://content.guardianapis.com/search', [
+                'q' => $preferences,
+                'show-elements' => 'image',
+                'page-size' => 15,
+                'api-key' => $this->api_keys['guardian']
+            ]);
+        });
+
+
         return response()->json([
             'status' => 200,
-            'message' => '',
-            'data' => []
+            'message' => 'Your personalized news feed!',
+            'data' => $this->getParsedResponse( $responses )
         ]);
     }
 }
