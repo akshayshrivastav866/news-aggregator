5752b357ab4a27d10f2fc3036573911693ae4ccf
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-04-24 17:51:37 +0530

    BE - Updated Preferences Controller

--- backend/app/Http/Controllers/Api/PreferencesController.php
+++ backend/app/Http/Controllers/Api/PreferencesController.php
@@ -6,8 +6,29 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\UserPreferences;
+use Illuminate\Support\Facades\Http;
 
 class PreferencesController extends Controller {
+    private function defined_preferences () {
+        $response = Http::timeout(20)->get('https://newsapi.org/v2/top-headlines/sources', [
+            'apiKey' => $this->api_keys['newsapi']
+        ]);
+
+        if ( $response->successful() ) {
+            foreach ( $response['sources'] as $source ) {
+                $categories[] = $source['category'];
+                $sources[] = $source['name'];
+            }
+        }
+
+        return [
+            'categories' => array_values(array_unique($categories)),
+            'authors' => ['Javier Lacort', 'Uriel Bederman', 'Simon Cohen'],
+            'sources' => array_values(array_unique($sources)),
+        ];
+
+    }
+
     public function getPreferences() {
         $data = [];
         // We will check if this user has already set user preference or not
@@ -15,14 +36,17 @@ class PreferencesController extends Controller {
 
         // If not data found for current user, i.e user is new and we will send default values.
         if ( ! $userPreferences ) {
-            $data = $this->defined_preferences;
+            $data = [
+                'selected' => [],
+                'defined' => $this->defined_preferences(),
+            ];
         }
 
         // If data is found for current user, i.e user is old and we will send previous selected preferences
         if ( ! empty( $userPreferences->preferences ) ) {
             $data = [
                 'selected' => $userPreferences->preferences,
-                'defined' => $this->defined_preferences,
+                'defined' => $this->defined_preferences(),
             ];
         }
 
@@ -30,14 +54,6 @@ class PreferencesController extends Controller {
             'status' => 200,
             'message' => 'User Preferences!',
             'data' => $data,
-        ]);
-    }
-
-    public function getFilterPreferences() {
-        return response()->json([
-            'status' => 200,
-            'message' => 'User Preferences!',
-            'data' => $this->defined_preferences,
         ]);
     }
 
